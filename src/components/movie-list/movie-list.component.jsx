9578414c8b97926fd7821f00667f3cb49ab10170
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-05-11 13:44:28 +0200

    included movie details component in movies list component

--- src/components/movie-list/movie-list.component.jsx
+++ src/components/movie-list/movie-list.component.jsx
@@ -1,16 +1,21 @@
 import './movie-list.styles.css'
 import MovieCard from '../movie-card/movie-card.component'
-import { AppContext } from '../../App'
+
+import { MovieDetailsContext } from '../../contexts/sidebar/movieDetails.context'
 import { useContext } from 'react'
+import MovieDetails from '../movie-details/movie-details.component'
 
 function MovieList({movies}){
+    const {movieDetailsState} = useContext(MovieDetailsContext)
+    console.log(movieDetailsState)
     return (
             <div className='movies-list'>
                 {   
                     movies.map(movie=>{
-                       return <MovieCard key={movie.id} poster={movie.poster_path} title={movie.original_title}/>
+                       return <MovieCard key={movie.id} poster={movie.poster_path} title={movie.original_title} id={movie.id}/>
                     })
                 }
+                {movieDetailsState.showDetails === true && <MovieDetails movieID={movieDetailsState.movieID}/>}
             </div>
         )
     

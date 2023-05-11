951b7b8b11f59cfd774bef8f5edd39c74a3bb82d
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-05-11 16:54:40 +0200

    fixed bugs in the movie details component, added new features to the component and edited styling

--- src/components/movie-card/movie-card.component.jsx
+++ src/components/movie-card/movie-card.component.jsx
@@ -1,9 +1,17 @@
+import { useContext } from 'react'
+import { MovieDetailsContext } from '../../contexts/sidebar/movieDetails.context'
 import './movie-card.styles.css'
 import { useState } from "react"
 
+
+
 function MovieCard(props){
+    const {setMovieDetailsState} = useContext(MovieDetailsContext)
     return(
-        <div onClick={()=>{}} className='movie-card'>
+        <div onClick={()=>{
+            setMovieDetailsState({showDetails:true,movieID:props.MovieID})
+        }}
+        className='movie-card'>
             <img src={`https://image.tmdb.org/t/p/original/${props.poster}`} alt={props.title} className="movie-thumbnail"/>
             <p className="movie-card-title">{props.title}</p>
         </div>
--- src/components/movie-details/movie-details.component.jsx
+++ src/components/movie-details/movie-details.component.jsx
@@ -1,37 +1,75 @@
 import './movie-details.style.css'
 import { fetchMovieById } from '../../fetchers/fetchers'
 import { fetchMovieTrailer } from '../../fetchers/fetchers'
-import useSWR  from 'swr'
-import close_icon from '../../assets/x_icon.svg'
+import { useContext } from 'react'
+import { MovieDetailsContext } from '../../contexts/sidebar/movieDetails.context'
+import  useSWR  from 'swr'
+import {ReactComponent as CloseIcon} from '../../assets/x_icon.svg'
 
 function MovieDetails({movieID}){
-        const {data:movie_details,error,isLoading} = useSWR(movieID,fetchMovieById,{
-            revalidateOnFocus:false,
-        })
+        const {setMovieDetailsState} = useContext(MovieDetailsContext)
+
+        function closeMovieDetails(){
+            setMovieDetailsState({showDetails:false,movieID:null})
+        }
+    
+
+        const {data:movie_details,error,isLoading} = useSWR(movieID,fetchMovieById);
+
         const {data:trailer_url} = useSWR(movieID,fetchMovieTrailer)
+        
         if(isLoading){
+            console.log('loading')
             return(
-            <div className='movie-details-container'> 
-                <button><img src={close_icon} alt="close window"/></button>
+            <div className='movie-details-container' onClick={closeMovieDetails}> 
+                <div className='controls'>
+                        <button className='close-btn' onClick={closeMovieDetails}>
+                            <CloseIcon/>
+                        </button>
+                        <button className='close-btn' onClick={closeMovieDetails}>
+                            <CloseIcon/>
+                        </button>
+                    </div>
                 <p className='loading'>loading</p>
             </div>
             )
         }
-        else if(error){
+        if(error){
             console.log(error)
             return(
-                <div className='movie-details-container'>
-                    <button><img src={close_icon} alt="close window"/></button> 
+                <div className='movie-details-container' onClick={closeMovieDetails}>
+                   <div className='controls'>
+                        <button className='close-btn' onClick={closeMovieDetails}>
+                            <CloseIcon/>
+                        </button>
+                        <button className='close-btn' onClick={closeMovieDetails}>
+                            <CloseIcon/>
+                        </button>
+                    </div>
                     <p className='loading'>error</p>
                 </div>
                 )
         }
-        else{
+        if(movie_details){
+            console.log(movie_details)
             return(
                 <div className='movie-details-container'>
-                    <button className='close-btn'><img src={close_icon} alt="close window"/></button>
-                    <div className='movie-image-container'>
-                        <img src="" alt="" />
+                    <div className='controls'>
+                        <button className='close-btn' onClick={closeMovieDetails}>
+                            <CloseIcon/>
+                        </button>
+                        <button className='close-btn' onClick={closeMovieDetails}>
+                            <CloseIcon/>
+                        </button>
+                    </div>
+                  
+                    <div className='movie-background-container'>
+                       <img src={`https://image.tmdb.org/t/p/original/${movie_details.backdrop_path}`} alt="" />
+                       <h2 className='movie-title'>{movie_details.original_title}</h2>
+                    </div>
+                    <div className='movie details'>
+                       <p className='overview'>Overview</p>
+                       <p className='overview-details'>{movie_details.overview}</p>
                     </div>
                 </div>
             )

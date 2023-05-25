286f466a8d022758a68932ecf023a35924e995ec
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-05-25 20:54:03 +0200

    added error handler for images added error handler for images so as to improver user experience in cases where an image for movie fails to load

--- src/components/movie-card/movie-card.component.jsx
+++ src/components/movie-card/movie-card.component.jsx
@@ -2,18 +2,29 @@ import { useContext } from 'react'
 import { imageUrlGenerator } from '../../URL-generators/url-genrators'
 import { MovieDetailsContext } from '../../contexts/sidebar/movieDetails.context'
 import './movie-card.styles.css'
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { useNavigate } from 'react-router-dom'
 
 function MovieCard(props){
+    const imgContainerRef = useRef(null) 
     const navigate = useNavigate()
     const {setMovieDetailsState} = useContext(MovieDetailsContext)
+    
+    //on error loading image event handler
+    function imgErrorHandler(event){
+        imgContainerRef.current.className = 'image-fallback'
+        // hide the alt value in the image but still allow 
+        // screen readers to understand the unloaded image 
+        event.target.style.display = 'none'
+    }
     return(
         <div onClick={()=>{
            navigate(`/movie-details/${props.movieID}`)
         }}
         className='movie-card'>
-            <img src={imageUrlGenerator(props.poster)} alt={props.title} className="movie-thumbnail"/>
+                <div ref={imgContainerRef}>
+                <img src={imageUrlGenerator(props.poster)} alt={props.title} className="movie-thumbnail" onError={imgErrorHandler}/>
+                </div>
             <p className="movie-card-title">{props.title}</p>
         </div>
     )

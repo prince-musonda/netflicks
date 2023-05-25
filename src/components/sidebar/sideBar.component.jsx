9c14a11d3c066c50e4e0ea25c91b80f6de1592f8
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-05-25 13:21:17 +0200

    refactored code and improved sideBar clicking

--- src/components/sidebar/sideBar.component.jsx
+++ src/components/sidebar/sideBar.component.jsx
@@ -3,6 +3,27 @@ import { SideBarContext } from '../../contexts/sidebar/sideBar.context'
 import { useContext } from 'react'
 import { Link} from 'react-router-dom'
 
+
+const sideBarOptions = [
+    {name: 'Home',path: '/'},
+    {name: 'Crime Movies',path: '/genre?id1=80'},
+    {name: 'Anime',path: '/genre?id1=16'},
+    {name: 'Drama',path: '/genre?id1=18'},
+    {name: 'Thriller',path: '/genre?id1=53'},
+    {name: 'Comedy',path: '/genre?id1=35'},
+    {name: 'Horror Movies',path: '/genre?id1=27'},
+    {name: 'Kids and Family Movies',path: '/genre?id1=10751'},
+    {name: 'Romantic Movies',path: '/genre?id1=10749'},
+    {name: 'History',path: '/genre?id1=36'},
+    {name: 'TV Shows',path: '/genre?id1=10770'},
+    {name: 'Sci-Fi and Fantasy',path: '/genre?id1878&id2=14'},
+    {name: 'Music and Musical',path: '/genre?id1=10402'},
+    {name: 'Action and Adventure',path: '/genre?id1=28&id2=12'},
+    {name: 'Documentaries',path: '/genre?id1=99'},
+    
+
+]
+
 function SideBar(){
     const {setShowSideBar} = useContext(SideBarContext)
     function HideSideBar(){
@@ -16,7 +37,12 @@ function SideBar(){
               <aside className="side-bar">
                 <p className='logo'>Netflicks</p>
                 <ul>
-                    <li><Link to='/'>Home</Link></li>
+                    {
+                        sideBarOptions.map(option => {
+                            return <li onClick={HideSideBar}><Link to={option.path}>{option.name}</Link></li>
+                        })
+                    }
+                    {/* <li><Link to='/'>Home</Link></li>
                     <li><Link to='/genre?id1=80'>Crime Movies</Link></li>
                     <li><Link to='/genre?id1=16'>Anime</Link></li>
                     <li><Link to='/genre?id1=18'>Dramas</Link></li>
@@ -31,7 +57,7 @@ function SideBar(){
                     <li><Link to='/genre?id1=10749'>Romantic Movies</Link></li>
                     <li><Link to='/genre?id1=10402'>Music & Musical</Link></li>
                     <li><Link to='/genre?id1=28&id2=12'>Action & Adventure</Link></li>
-                    <li><Link to='/genre?id1=99'>Documentaries</Link></li>
+                    <li><Link to='/genre?id1=99'>Documentaries</Link></li> */}
 
                 </ul>
               </aside>

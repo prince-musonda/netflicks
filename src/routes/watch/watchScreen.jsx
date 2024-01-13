571bfd05659afdfaa14aad2d40654765880b20c4
Prince Musonda <princemusonda5840@gmail.com>
2024-01-13 16:50:41 +0200

    edited styling

--- src/routes/watch/watchScreen.jsx
+++ src/routes/watch/watchScreen.jsx
@@ -3,6 +3,14 @@ import SWR from "swr";
 import { getStreamableTrailerUrl } from "../../fetchers/fetchers";
 import "./style.css";
 
+function CustomVideoPlayer(props) {
+  return (
+    <div className="video_container">
+      <video src={props.src} controls className="video_player"></video>
+    </div>
+  );
+}
+
 export default function WatchScreen() {
   const { trailerId: youtubeVideoId } = useParams();
   const {
@@ -16,9 +24,7 @@ export default function WatchScreen() {
     return <p className="loading">Loading</p>;
   } else if (streamableVideoUrl) {
     console.log(streamableVideoUrl);
-    return (
-      <video src={streamableVideoUrl} controls className="video_player"></video>
-    );
+    return <CustomVideoPlayer src={streamableVideoUrl} />;
   } else if (isError) {
     return <p className="loading">{error.message}</p>;
   }
